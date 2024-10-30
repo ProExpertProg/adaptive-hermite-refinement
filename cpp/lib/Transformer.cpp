9853eaf639241fab972c794373c91391082d1de7
Luka Govedic <luka.govedic@gmail.com>
2024-10-30 17:44:58 -0400

    Formatting

--- cpp/lib/Transformer.cpp
+++ cpp/lib/Transformer.cpp
@@ -5,12 +5,13 @@
 #include "Transformer.hpp"
 
 namespace ahr {
-void ahr::Transformer::init() {
+void Transformer::init() {
   auto r = grid.rBufXY();
   auto c = grid.cBufXY();
   fftFwd = fftw::plan_r2c<2u>::dft(r.to_mdspan(), c.to_mdspan(), fftw::ESTIMATE);
   fftBwd = fftw::plan_c2r<2u>::dft(c.to_mdspan(), r.to_mdspan(), fftw::ESTIMATE);
 }
+
 void Transformer::fft(Grid::View::R_XY in, Grid::View::C_XY out) const { fftFwd(in, out); }
 
 void Transformer::bfft(Grid::View::C_XY in, Grid::View::R_XY out) const { fftBwd(in, out); }
