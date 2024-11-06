d02d7a77ea044dcb4c446dfb92495d5c644519ad
Luka Govedic <luka.govedic@gmail.com>
2024-11-06 17:09:26 -0500

    Remove logging from benchmark

---
+++ cpp/bench/main-no-log.cpp
@@ -0,0 +1,14 @@
+#include <benchmark/benchmark.h>
+#include <spdlog/cfg/env.h>
+#include <spdlog/spdlog.h>
+
+
+int main(int argc, char **argv) {
+  // No logging in benchmarks (unless overridden via environment variable)
+  spdlog::set_level(spdlog::level::off);
+  spdlog::cfg::load_env_levels();
+
+  benchmark::Initialize(&argc, argv);
+  benchmark::RunSpecifiedBenchmarks();
+  benchmark::Shutdown();
+}
