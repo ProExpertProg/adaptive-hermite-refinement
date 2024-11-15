b9aa6798f3662d90b9f709cff6215c1d1e1fc85c
Luka Govedic <luka.govedic@gmail.com>
2024-11-15 11:44:35 -0500

    Add vectorized PrepareDerivatives, test, and benchmark

--- cpp/lib/PrepareDerivatives.hpp
+++ cpp/lib/PrepareDerivatives.hpp
@@ -2,10 +2,12 @@
 
 #include "constants.hpp"
 #include "grid.hpp"
+#include <eve/wide.hpp>
 
 namespace ahr {
 
 class PrepareDerivatives {
+protected:
   using View = Grid::View;
   template <class T> using DxDy = Grid::DxDy<T>;
 
@@ -14,7 +16,7 @@ public:
 
   void operator()(View::C_XY const &in, DxDy<View::C_XY> out) const;
 
-private:
+protected:
   Grid const &grid;
 
   /// Normalization factor for FFT
@@ -26,4 +28,22 @@ private:
   }
   [[nodiscard]] Real kx_(Dim kx) const { return Real(kx); }
 };
+
+class PrepareDerivativesVector : protected PrepareDerivatives {
+public:
+  explicit PrepareDerivativesVector(Grid const &grid) : PrepareDerivatives(grid) {}
+
+  void operator()(View::C_XY const &in, DxDy<View::C_XY> out) const;
+
+protected:
+  using VReal = eve::wide<Real>;
+
+  // TODO vec_utils
+  using VIdx = eve::wide<long long>;
+  static auto constexpr R_WIDTH = VReal::size();
+  static auto constexpr C_WIDTH = VReal::size() / 2;
+
+  static constexpr auto KY_TILE = 4;
+};
+
 } // namespace ahr
