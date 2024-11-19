0f09ef745491383e01b89d9f840b043f3e3b897d
Luka Govedic <luka.govedic@gmail.com>
2024-11-18 19:57:09 -0500

    constexpr for constants

--- cpp/lib/constants.hpp
+++ cpp/lib/constants.hpp
@@ -11,38 +11,38 @@ constexpr Real ly = 1.0 * 2 * pi;
 // TODO organize these into struct instead of global
 
 /// Time parameters
-inline Real InitAA0Fac = 0.1;
+constexpr Real InitAA0Fac = 0.1;
 inline Real CFLFrac = 0.2;
-inline Real epsilon = 1e-10;
+constexpr Real epsilon = 1e-10;
 
 // This actually means MaxP+1 is the last computed value
-inline Dim MaxP = 1;
+constexpr Dim MaxP = 1;
 
-inline Real aa0 = InitAA0Fac; /// TODO not const
-inline Real low = 0.92;
+constexpr Real aa0 = InitAA0Fac; /// TODO not const
+constexpr Real low = 0.92;
 
 /// FLR
-inline Real rhoI = 1.0e-7;
-inline Real rhoS = 1.0e-7;
-inline Real de = 1.0e-7;
+constexpr Real rhoI = 1.0e-7;
+constexpr Real rhoS = 1.0e-7;
+constexpr Real de = 1.0e-7;
 
 /// MHD
-inline Real smallRhoI = 1.0e-6;
+constexpr Real smallRhoI = 1.0e-6;
 
 /// Diffusion
-inline Real nu_ei = 0.0001;
+constexpr Real nu_ei = 0.0001;
 inline Real res = 0.1;
 inline Real nu = 0.1; // This is niu in Viriato
-inline Real hyper_coef_g = 0.0;
-inline Real hyper_coef = 0.0;
-inline Real hyperm_coef = 0.0;
+constexpr Real hyper_coef_g = 0.0;
+constexpr Real hyper_coef = 0.0;
+constexpr Real hyperm_coef = 0.0;
 
-inline Dim hyper_order = 3;
-inline Dim hyper_order_g = 3;
-inline Dim hyper_morder = 3;
+constexpr Dim hyper_order = 3;
+constexpr Dim hyper_order_g = 3;
+constexpr Dim hyper_morder = 3;
 
 /// equil
-inline Real a0 = 1.0;
+constexpr Real a0 = 1.0;
 
 struct HyperCoefficients {
   Real nu_g, nu_2, eta2, nu_ei;
